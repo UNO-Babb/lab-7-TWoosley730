193ae0479219d4bcd18c9529ff51970b2e56b2d2
TWoosley730 <twoosley@unomaha.edu>
2025-03-09 23:04:36 -0500

    Update Problem 10.py

--- Problem 10.py
+++ Problem 10.py
@@ -1,11 +1,17 @@
 #Problem10.py
 #Project problem 10
 
-from NumberTests import isEven
+from NumberTests import isPrime
 from NumberTests import fibonacciSequence
 
 def main():
-  
+  def sum_of_primes(limit):
+    """Return the summation of all prime numbers less than the given limit."""
+    total = 0
+    for num in range(2, limit):
+        if is_prime(num):
+            total += num
+    return total
   
   
 
